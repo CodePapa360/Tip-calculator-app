63259fead04ab644e0eeec731ea80e74601cc08c
Alamin <codepapa360@gmail.com>
2023-06-29 17:56:15 +0600

    Improved codes

--- src/js/index.js
+++ src/js/index.js
@@ -1,100 +1,118 @@
 "use strict";
 import "../sass/main.scss";
 
-const inpBill = document.getElementById("bill");
-const parcents = document.querySelector(".percentages-container");
-const inpCustom = document.querySelector(".input-custom");
-const inpPerson = document.getElementById("persons");
-const allRadio = document.querySelectorAll(".radio-parcent");
-const errorMsgEl = document.querySelector(".error-msg");
-const btnReset = document.querySelector(".btn-reset");
+(function () {
+  const inpBill = document.getElementById("bill");
+  const parcents = document.querySelector(".percentages-container");
+  const inpCustom = document.querySelector(".input-custom");
+  const inpPerson = document.getElementById("persons");
+  const allRadio = document.querySelectorAll(".radio-parcent");
+  const errorMsgEl = document.querySelector(".error-msg");
+  const btnReset = document.querySelector(".btn-reset");
 
-const outputTip = document.getElementById("tip");
-const outputTotal = document.getElementById("total");
+  const outputTip = document.getElementById("tip");
+  const outputTotal = document.getElementById("total");
 
-///////////////
-// Tip calcutor
-const calcTip = function (bill, percent, person) {
-  const totalTip = (bill * percent) / 100;
-  const tip = totalTip / person;
-  const total = (bill + totalTip) / person;
+  ///////////////
+  // Tip calcutor
+  const calcTip = function (bill, percent, person) {
+    const totalTip = (bill * percent) / 100;
+    const tip = totalTip / person;
+    const total = (bill + totalTip) / person;
 
-  return { tip: tip, total: total };
-};
+    return { tip: tip, total: total };
+  };
 
-/////////////////
-////////////////
-let curBill, curPers, curPerson;
+  /////////////////
+  ////////////////
 
-const verifier = function () {
-  if (!curBill || !curBill || !curPerson) return;
+  let curBill, curPers, curPerson;
 
-  renderOutput(calcTip(curBill, curPers, curPerson));
-};
+  ////////////////
+  /////Verifier
+  const verifier = function () {
+    if (
+      curBill === undefined ||
+      curBill === undefined ||
+      curPerson === undefined
+    )
+      return;
 
-////////////////
-parcents.addEventListener("click", function (event) {
-  const radioEl = event.target.closest(".radio-parcent");
-  if (!radioEl) return;
+    renderOutput(calcTip(curBill, curPers, curPerson));
+  };
 
-  curPers = +radioEl.value;
-  inpCustom.value = "";
+  ////////////////
+  //percentage input
+  parcents.addEventListener("click", function (event) {
+    const radioEl = event.target.closest(".radio-parcent");
+    if (!radioEl) return;
 
-  verifier();
-});
+    curPers = +radioEl.value;
+    inpCustom.value = "";
 
-//////////////////
-inpCustom.addEventListener("input", function () {
-  curPers = +inpCustom.value;
-  allRadio.forEach((radio) => {
-    if (radio.checked) radio.checked = false;
+    verifier();
   });
 
-  verifier();
-});
+  //////////////////
+  // Custom percentage input
+  inpCustom.addEventListener("input", function () {
+    curPers = +inpCustom.value;
+    allRadio.forEach((radio) => {
+      if (radio.checked) radio.checked = false;
+    });
 
-//////////////////
-inpBill.addEventListener("input", function () {
-  curBill = +inpBill.value;
+    verifier();
+  });
 
-  verifier();
-});
+  //////////////////
+  //Bill input
+  inpBill.addEventListener("input", function () {
+    curBill = +inpBill.value;
 
-inpPerson.addEventListener("input", function () {
-  if (+inpPerson.value < 1) {
-    inpPerson.style.borderColor = "var(--red)";
-    errorMsgEl.textContent = "Can't be zero";
-    return;
-  }
+    verifier();
+  });
 
-  inpPerson.style.borderColor = null;
-  errorMsgEl.textContent = "";
-  curPerson = +inpPerson.value;
+  ////////////////////////
+  // Input number of peopel
+  inpPerson.addEventListener("input", function () {
+    if (+inpPerson.value < 1) {
+      inpPerson.style.outline = "2px solid var(--red)";
+      errorMsgEl.textContent = "Can't be zero";
+      return;
+    }
 
-  verifier();
-});
+    inpPerson.style.outline = null;
+    errorMsgEl.textContent = "";
+    curPerson = +inpPerson.value;
 
-/////////////
-const renderOutput = function (obj) {
-  outputTip.textContent = obj.tip.toFixed(2);
-  outputTotal.textContent = obj.total.toFixed(2);
-};
+    verifier();
+  });
 
-//reset funtionality
-const reset = function () {
-  inpBill.value = "";
-  inpCustom.value = "";
-  inpPerson.value = "";
-  curBill = undefined;
-  curPers = undefined;
-  curPerson = undefined;
+  ////////////////////////////
+  // Rendering the output
+  const renderOutput = function (obj) {
+    outputTip.textContent = obj.tip.toFixed(2);
+    outputTotal.textContent = obj.total.toFixed(2);
+  };
 
-  allRadio.forEach((rad) => (rad.checked = false));
-  renderOutput(calcTip(0, 0, 1));
-};
+  ///////////////////////
+  //reset funtionality
+  const reset = function () {
+    inpBill.value = "";
+    inpCustom.value = "";
+    inpPerson.value = "";
+    curBill = undefined;
+    curPers = undefined;
+    curPerson = undefined;
 
-reset();
+    allRadio.forEach((rad) => (rad.checked = false));
+    renderOutput(calcTip(0, 0, 1));
+  };
+  reset(); // Calling immediately after reloading the page.
 
-btnReset.addEventListener("click", function () {
-  reset();
-});
+  ////////////////////
+  //Reset button listener
+  btnReset.addEventListener("click", function () {
+    reset();
+  });
+})();
